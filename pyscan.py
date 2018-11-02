98dcc26a1a53e2c6b2337695556d339b4d3bd7b8
jsalisi <jsalisi@gmail.com>
2018-11-02 12:04:43 -0700

    initial commit

--- pyscan.py
+++ pyscan.py
@@ -1,5 +1,3 @@
-# pyscan
-
 # Features:
-# Email Parser - collects .ply scans from an email
+# Email Parser - gathers .ply scans from an email
 # Object Classifier - loads .ply file and returns the shape of the object
